a6063baa25aed02255fbee3931cf893296ab27b5
Dipro Chowdhury <achowdh1@mail.sfsu.edu>
2018-08-14 23:32:47 -0700

    test code error resolved

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/EndspointsAsyncTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/EndspointsAsyncTaskTest.java
@@ -1,5 +1,7 @@
 package com.udacity.gradle.builditbigger;
 
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 
 import org.junit.Rule;
@@ -7,10 +9,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.not;
 
 @RunWith(AndroidJUnit4.class)
-@LargeTest
 public class EndspointsAsyncTaskTest {
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
